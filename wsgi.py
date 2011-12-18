8dd29aa6df764b02be690ef29bafaa20e6497e3c
xando <sebastian.pawlus@gmail.com>
2011-12-18 13:12:17 +0100

    wsgi file added.

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+virtualenv = os.path.join(os.path.dirname(__file__), ".virtualenv/bin/activate_this.py")
+execfile(virtualenv, dict(__file__=virtualenv))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "progpac.settings"
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
