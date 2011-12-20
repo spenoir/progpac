663584dc3434796861dbe391bae00466e0be9fa0
xando <sebastian.pawlus@gmail.com>
2011-12-21 00:07:37 +0100

    Some modification after merge from Kuba.

--- views.py
+++ views.py
@@ -10,32 +10,24 @@ from progpac import h_language
 class Home(FormView):
     template_name = "base.html"
     form_class = forms.Editor
-    initial = {
-        'text': "slssrssrssssrss",
-    }
-
-    def read_level(self, name):
-        lines = []
-        for line in open(join(settings.SITE_ROOT, 'levels', name)):
-            line = line.rstrip()
-            if line.startswith('.'):
-                lines.append(list(line))
-        return lines
+    initial = { 'text': "sssrss" }
 
     def get_context_data(self, *args, **kwargs):
-        ctx = super(Home, self).get_context_data(*args, **kwargs)
-        ctx['level'] = json.dumps(self.read_level('level1.txt'))
-        return ctx
+        context = super(Home, self).get_context_data(*args, **kwargs)
+        level_lines = open(join(
+            settings.SITE_ROOT, 'levels', 'level1.txt')).readlines()[:25]
+        context["level"] = json.dumps(
+            [map(str, line.rstrip()) for line in level_lines])
+        return context
 
     def form_valid(self, form):
         parser = h_language.Parser(form.cleaned_data['text'])
 
-        context = self.get_context_data()
-        context.update({
+        context = {
             "form": form,
             "error": parser.error,
             "code": parser.code,
-        })
+        }
 
         if self.request.POST['submit'] == 'Debug':
             context.update({
@@ -43,4 +35,5 @@ class Home(FormView):
                 "debug_ast": parser.ast,
             })
 
-        return self.render_to_response(context)
+        return self.render_to_response(
+            self.get_context_data(**context))
