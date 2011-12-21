d65aef7a93704b928ccb28e97f5db348650d4c84
xando <sebastian.pawlus@gmail.com>
2011-12-22 00:00:11 +0100

    load_level fix

--- load_levels.py
+++ load_levels.py
@@ -16,13 +16,14 @@ def sorted_nicely( l ):
 
 if len(sys.argv) > 1:
     directory = sys.argv[1]
+    Level.objects.all().delete()
     for f in sorted_nicely(os.listdir(sys.argv[1])):
         level_lines = open(os.path.join(directory, f)).readlines()[:25]
         content = [line.rstrip() for line in level_lines]
         name = f.rstrip(".txt")
         content = "\n".join(content)
 
-        Level.objects.all().delete()
+
         
         Level.objects.create(
             name=name,
