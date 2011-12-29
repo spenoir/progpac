aed2d3ad6d6faaae3be255aad6477b7258aa4710
xando <sebastian.pawlus@gmail.com>
2011-12-29 22:58:56 +0100

    New nice counters.

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,26 @@
-function count_code() {
+function setup_code_counter() {
+    var $code_size = $('.code-size');
+    var $code_maxsize = $('.code-maxsize');
     var code = $('.editor textarea').val();
 
-    return $.map(code.split("\n"), function(element) {
+    var code_length = $.map(code.split("\n"), function(element) {
         return element.replace(/\s/g,"")
     }).join("").length;
+
+    $code_size.html(code_length);
+
+    if ($code_size.html() > $code_maxsize.html()) {
+        $code_size.css('color', "#B94A48")
+    } else {
+        $code_size.css('color', "#468847")
+    }
 }
 
 $(document).ready(function() {
-    var $code_counter = $('.code-size')
-    
-    $code_counter.html(count_code());
+    setup_code_counter();
 
-    $editor.on('keyup', function() { 
-        $code_counter.html(count_code());
+    $('.editor textarea').on('keyup', function() { 
+        setup_code_counter();
     });
 
     $('.modal').modal({
