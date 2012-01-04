9072ae50fdb2d7211b9240ea1490372c694a54a4
xando <sebastian.pawlus@gmail.com>
2012-01-04 17:02:52 +0100

    Template tricks to handle active part in menubar.

--- settings.py
+++ settings.py
@@ -103,6 +103,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
+    "django.core.context_processors.request",
     "django.core.context_processors.static",
     "django.contrib.messages.context_processors.messages",
     "progpac.context_processors.default"
