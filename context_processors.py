7d3e47400aab586b9369e76000be399a728d8932
xando <sebastian.pawlus@gmail.com>
2012-01-17 21:53:54 +0000

    Fixed bug with going to the site for a first time with some hash.

--- context_processors.py
+++ context_processors.py
@@ -2,7 +2,10 @@ def default(request):
     from progpac.models import Level
 
     last_level_hash = request.session.get('last_level_hash')
-    last_level = Level.objects.get(hash=last_level_hash)
+    if last_level_hash:
+        last_level = Level.objects.get(hash=last_level_hash)
+    else:
+        last_level = Level.objects.all()[0]
 
     return {
         'previous_levels': last_level.all_previous()
